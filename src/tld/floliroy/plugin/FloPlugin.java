f449b288e18c481e335445605c8b82158c647e10
Flo Leeroy <florian.leeroy@hotmail.com>
2020-03-25 17:29:55 +0100

    with logger

--- src/tld/floliroy/plugin/FloPlugin.java
+++ src/tld/floliroy/plugin/FloPlugin.java
@@ -8,19 +8,16 @@ public class FloPlugin extends JavaPlugin {
 	
 	@Override
 	public void onEnable() {
-		System.out.println("Je démarre");
+		getLogger().info("Je démarre");
 	}
 	
 	@Override
 	public void onDisable() {
-		System.out.println("Je m'éteint");
+		getLogger().info("Je m'éteint");
 	}
 	
 	@Override
-    public boolean onCommand(CommandSender sender,
-                             Command command,
-                             String label,
-                             String[] args) {
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         if (command.getName().equalsIgnoreCase("mycommand")) {
             sender.sendMessage("You ran /mycommand!");
             return true;
